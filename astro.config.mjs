d2a23dd6f9542dc154990585c457899980e9aba6
whit3st <alican15033@gmail.com>
2024-03-09 16:05:06 +0300

    added sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'ignore',
+  trailingSlash: "ignore",
   build: {},
-  output: 'hybrid',
+  output: "hybrid",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
 });
