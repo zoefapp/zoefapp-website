61afe8e7f35124f22851d500991dc6232b6e7c20
whit3st <alican15033@gmail.com>
2024-03-11 01:30:28 +0300

    improved themes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,5 @@ import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
