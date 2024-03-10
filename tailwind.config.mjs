2786e1b6b6deb1799c4a08916ef911b4732d00f4
whit3st <alican15033@gmail.com>
2024-03-10 04:35:48 +0300

    added font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        "mulish": ["Mulish", "sans-serif"],
+      },
       screens: {
         "2xl": "1400px",
       },
