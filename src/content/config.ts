e250d14753dfef802fd1a2d83df9d5ad8ca79e61
Sander de Groot <sander@degroot.dev>
2024-03-11 19:16:23 +0100

    feat(faq): added faq query possibility feat(testimonial): added testimonial query possibility

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,9 @@
 import { defineCollection, z } from 'astro:content';
+import { FaqSchema } from '~/lib/faq';
+import { TestimonialSchema } from '~/lib/testimonial';
 
+
+/// BLOG
 const blogSchema = z.object({
   title: z.string(),
   author: z.string(),
@@ -8,17 +12,31 @@ const blogSchema = z.object({
   language: z.enum(['nl', 'en']), // todo: use SUPPORTED_LANGUAGE˜S
   tags: z.array(z.string()),
 })
+export type Blog = z.infer<typeof blogSchema>;
 
 const blogCollection = defineCollection({
   type: 'content',
   schema: blogSchema
 });
-export type Blog = z.infer<typeof blogSchema>;
 
+/// FAQ
+const faqCollection = defineCollection({
+  type: 'data',
+  schema: FaqSchema,
+})
+
+/// FAQ
+const testimonialCollection = defineCollection({
+  type: 'data',
+  schema: TestimonialSchema,
+})
+
+
+// EXPORTS
 export const collections = {
-  'blog': blogCollection
+  'blog': blogCollection,
+  'faq': faqCollection,
+  'testimonial': testimonialCollection
 };
-
-
 export type ContentCollection = keyof typeof collections
 
