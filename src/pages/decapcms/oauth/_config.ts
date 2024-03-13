001739075ddaf41fe3cd4013379a40dd1d2df4cb
Sander de Groot <sander@degroot.dev>
2024-03-13 16:52:44 +0100

    fix

--- src/pages/decapcms/oauth/_config.ts
+++ src/pages/decapcms/oauth/_config.ts
@@ -3,7 +3,7 @@ export const clientSecret = import.meta.env.DECAPCMS_OAUTH_CLIENT_SECRET ?? 'uns
 
 export const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`;
 export const tokenUrl = "https://github.com/login/oauth/access_token";
-export const githubOAuthRedirectUrl = 'https://get.zoef.app/decapcms/oauth/redirect'
+export const githubOAuthRedirectUrl = 'https://get.zoef.app/decapcms/oauth/forward'
 
 export const BRANCH_URL = import.meta.env.VERCEL_BRANCH_URL ? `https://${import.meta.env.VERCEL_BRANCH_URL}` : "http://localhost:4321";
 export const BRANCH_HOSTNAME = import.meta.env.VERCEL_BRANCH_URL ? import.meta.env.VERCEL_BRANCH_URL : "localhost:4321";
