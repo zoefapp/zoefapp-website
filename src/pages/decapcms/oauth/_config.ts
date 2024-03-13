fe9ac8d34818fe4c1b5828f9152ac5219c77a8c7
Sander de Groot <sander@degroot.dev>
2024-03-13 16:48:15 +0100

    feat(decapcms): move github credentials to environment variables feat(decapcms): redirect always through get.zoef.app - later to be replaced with the /decapcms/oauth/forward endpoint

--- src/pages/decapcms/oauth/_config.ts
+++ src/pages/decapcms/oauth/_config.ts
@@ -1,8 +1,9 @@
-export const clientId = "2857faab313f81362680"
-export const clientSecret = "fd67c7b0eaab908df26d6c2c300a765be4a04055"
+export const clientId = import.meta.env.DECAPCMS_OAUTH_CLIENT_ID ?? 'unset-env-variable'
+export const clientSecret = import.meta.env.DECAPCMS_OAUTH_CLIENT_SECRET ?? 'unset-env-variable'
 
 export const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`;
 export const tokenUrl = "https://github.com/login/oauth/access_token";
+export const githubOAuthRedirectUrl = 'https://get.zoef.app/decapcms/oauth/redirect'
 
 export const BRANCH_URL = import.meta.env.VERCEL_BRANCH_URL ? `https://${import.meta.env.VERCEL_BRANCH_URL}` : "http://localhost:4321";
 export const BRANCH_HOSTNAME = import.meta.env.VERCEL_BRANCH_URL ? import.meta.env.VERCEL_BRANCH_URL : "localhost:4321";
--- src/pages/decapcms/oauth/index.ts
+++ src/pages/decapcms/oauth/index.ts
@@ -1,11 +1,12 @@
 import type { APIRoute } from "astro";
-import { BRANCH_URL, authUrl } from "./_config";
+import { BRANCH_HOSTNAME, authUrl, githubOAuthRedirectUrl } from "./_config";
 
 export const prerender = false;
 
+/**
+ * This route is called when the user clicks the "Login with GitHub" button.
+ */
 export const GET: APIRoute = ({ redirect, url }) => {
-  let redirectUrl = authUrl;
-  redirectUrl += `&redirect_uri=${BRANCH_URL}/decapcms/oauth/callback`;
-  console.log(redirectUrl)
-  return redirect(authUrl);
+  // aftwards, github will redirect us to our redirect function, which will then redirect to the correct hostname
+  return redirect(`${authUrl}&redirect_uri=${githubOAuthRedirectUrl}?domain=${BRANCH_HOSTNAME}`);
 };
