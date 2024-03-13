344154b7a5ca35c13f1807173db0b0cd147687e0
Sander de Groot <sander@degroot.dev>
2024-03-13 12:18:33 +0100

    feat(cms): added authentication provider for github

---
+++ src/pages/decapcms/oauth/_config.ts
@@ -0,0 +1,5 @@
+export const clientId = "2857faab313f81362680"
+export const clientSecret = "fd67c7b0eaab908df26d6c2c300a765be4a04055"
+
+export const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`;
+export const tokenUrl = "https://github.com/login/oauth/access_token";
