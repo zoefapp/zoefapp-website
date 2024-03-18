8f0db036e0fad50168a6e7a100b505a0c7ecd65a
Sander de Groot <sander@degroot.dev>
2024-03-18 12:58:54 +0100

    feat(alpine): added alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,19 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import htmx from 'astro-htmx';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    trailingSlash: "ignore",
-    build: {},
-    output: "hybrid",
-    adapter: vercel({
-        edgeMiddleware: true,
-    }),
-
-    integrations: [
-        tailwind({ applyBaseStyles: false }), 
-        htmx(),
-        icon()
-    ],
+  trailingSlash: "ignore",
+  build: {},
+  output: "hybrid",
+  adapter: vercel({
+    edgeMiddleware: true
+  }),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), htmx(), icon(), alpinejs()]
 });
