fb5f22b9bd49234308af1a3dbe1f34ee1ec05c25
Sander de Groot <sander@degroot.dev>
2024-06-08 17:53:35 +0200

    feat(content): added basics for adding content pages based on mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import htmx from 'astro-htmx';
+import htmx from "astro-htmx";
+import mdx from '@astrojs/mdx'
 import vercel from "@astrojs/vercel/serverless";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "ignore",
-  build: {},
-  output: "hybrid",
-  adapter: vercel({
-    edgeMiddleware: true
-  }),
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), htmx(), icon(), alpinejs()]
+    trailingSlash: "ignore",
+    build: {},
+    output: "hybrid",
+    adapter: vercel({
+        edgeMiddleware: true,
+    }),
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        mdx(),
+        htmx(),
+        icon(),
+        alpinejs(),
+    ],
 });
