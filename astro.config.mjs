bbb2be784f5e9ba4a419de53a44e4bc42b1d2e12
whit3st <alican15033@gmail.com>
2024-06-25 06:23:52 +0300

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,21 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import htmx from "astro-htmx";
-import mdx from '@astrojs/mdx'
+import mdx from '@astrojs/mdx';
 import vercel from "@astrojs/vercel/serverless";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    trailingSlash: "ignore",
-    build: {},
-    output: "hybrid",
-    adapter: vercel({
-        edgeMiddleware: true,
-    }),
-    integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        mdx({
-            
-        }),
-        htmx(),
-        icon(),
-        alpinejs(),
-    ],
+  trailingSlash: "ignore",
+  build: {},
+  output: "hybrid",
+  adapter: vercel({
+    edgeMiddleware: true
+  }),
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx({}), htmx(), icon(), alpinejs(), react()]
 });
