0f9d7554269dd073308f9c09e6f65b323b7795cd
whit3st <alican15033@gmail.com>
2024-06-25 08:59:21 +0300

    react button update

--- src/components/ui/RButton.tsx
+++ src/components/ui/RButton.tsx
@@ -1,7 +1,11 @@
 import type { ButtonHTMLAttributes } from "react";
 const RButton = ({ children, ...props }: ButtonHTMLAttributes<HTMLButtonElement> & { children: React.ReactNode }) => {
     return (
-        <button type="button" {...props} className="border rounded-md bg-skin-secondary px-4 py-1.5 text-black font-semibold ">
+        <button
+            type="button"
+            {...props}
+            className="border-gray-300 bg-white hover:bg-gray-100 active:bg-gray-200 px-4 py-1.5 border rounded-md font-semibold text-skin-base duration-200 trasition-colors"
+        >
             {children}
         </button>
     );
